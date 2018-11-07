7ec2d089a057220d878547243bbfe77b866f3de7
Hiyro92 <Hiyro92@gmail.com>
2018-11-07 18:39:14 +0100

    -Neues Reposetory

---
+++ recipe-online/src/main/java/de/deltaonline/RecipeOnlineApplication.java
@@ -0,0 +1,12 @@
+package de.deltaonline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeOnlineApplication.class, args);
+	}
+}
